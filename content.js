88a30f53935f8cc5d7d39caa42cc127e3ce5000f
Polydynamical <cbracketdash@gmail.com>
2023-10-04 21:21:34 -0700

    styling

--- content.js
+++ content.js
@@ -5,12 +5,21 @@ const handleSelection = (e) => {
 
     var fanyi = document.getElementById("fanyi");
     fanyi.innerText = window.getSelection().toString();
-    fanyi.style.width = "200px";
-    fanyi.style.height = "200px";
     fanyi.style.position = "absolute";
+    fanyi.style.borderStyle = "solid";
+    fanyi.style.borderRadius = "15px";
     fanyi.style.left = window.pageXOffset + e.clientX + "px";
     fanyi.style.top = window.pageYOffset + e.clientY + "px";
-    fanyi.style.background = "green";
+    fanyi.style.background = "#FFE599";
     fanyi.style.fontSize = "24px";
 }
+const handleDeselection = (e) => {
+    try {
+        const fanyi = document.getElementById("fanyi");
+        fanyi.remove();
+    } catch(err) {
+        ;
+    }
+}
 window.addEventListener("dblclick", handleSelection);
+window.addEventListener("click", handleDeselection);
